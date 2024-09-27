2c8fca55216e1e4c16ef77be07097a8647e2317b
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-09-27 13:31:33 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const app = express();
+const PORT = 5000 
+
+
+
+app.use(express.static('public'));
+
+//templating engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+
+
+app.use('/', require('./routes/main'));
+
+app.listen(PORT, () => {
+    console.log('App listening on port 5000');
+});
+
