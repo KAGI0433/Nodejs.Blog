1c0831998723327895a10e5bbd0ccc4a0d4fa5f4
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-09-28 15:12:38 +0200

    connect database

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
+const connectDB = require('./routes/config/db');
+
 const app = express();
 const PORT = 5000 
 
+//connect to DB
 
+connectDB();
 
 app.use(express.static('public'));
 
