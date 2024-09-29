8eb0f88234476f49ddd02c829cdfafc36e5cda49
KAGI0433 <168522964+KAGI0433@users.noreply.github.com>
2024-09-29 20:54:47 +0200

    save posts to database

--- routes/main.js
+++ routes/main.js
@@ -124,6 +124,24 @@ router.get('/newpost', (req, res) => {
 });
 
 
+router.post("/main", (req, res) => {
+    const { title, content } = req.body;
+    if (!title || !content) return res.send("Please enter all the required credentials!")
+
+        const newPost = new Post({title, content})
+
+        //save the post to the database
+
+        newPost.save()
+        .then(() => {
+            console.log("Post saved successfully!");
+            res.redirect('/')
+        })
+
+        .catch(err => console.log(err));
+});
+
+
 
 module.exports = router;
 
